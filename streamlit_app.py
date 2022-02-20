2f71f84b845581acd07774b6999c309ba5b28d6d
Giriteja <40452942+Giriteja@users.noreply.github.com>
2022-02-20 23:01:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def model():
     return bi_encoder,cross_encoder
 @st.cache    
 def data():
-    DF_text_time = pd.read_pickle(r"C:\Newdownloads\Covid19--master\search\Transcript\final_sent")
+    DF_text_time = pd.read_pickle(r"final_sent")
     embd=list(DF_text_time['Embeddings'])
     total_text_set_list=list(DF_text_time['Sentance_3_cleaned'])
     filename=list(DF_text_time['filename'])
